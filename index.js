6d47757306dd67e2963f6dd713af36415f4a897c
AJITH G <ajithhub@gmail.com>
2023-07-20 11:25:15 +0530

    port changed

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ app.get('/hey', (req, res) => {
 	res.json('This is the home page!');
 });
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Server started ${port}...`));
