61b9ecefffd4d8529a0c9d967d4850418b6d7a2a
AJITH G <ajithhub@gmail.com>
2023-07-20 10:44:48 +0530

    hey added

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get('/', (req, res) => {
 	res.send('This is the home page!');
 });
 
+app.get('/hey', (req, res) => {
+	res.send('This is the home page!');
+});
+
 const port = process.env.PORT || 8080;
 
 app.listen(port, () => console.log(`Server started ${port}...`));
